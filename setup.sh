67f1a3b7999cfd2d59b1969c9889df5e2ad8cfae
guxukai <1378026744@qq.com>
2019-10-08 21:29:49 +0800

    构建文件

--- setup.sh
+++ setup.sh
@@ -5,9 +5,9 @@ docker stop shop_admin || true;
 # 删除shop_admin容器
 docker rm shop_admin || true;
 # 删除shop/admin镜像
-docker rmi --force $(docker images | grep shop/admin | awk '{print $3}')
+docker rmi --force $(docker images | grep shop/admin | awk '{print $3}');
 # 构建shop/admin:$image_version镜像
-docker build . -t shop/admin:$image_version;
+docker build -t shop/admin:$image_version .;
 # 查看镜像列表
 docker images;
 # 基于shop/admin 镜像 构建一个容器 shop_admin
@@ -15,6 +15,6 @@ docker run -p 9527:80 -d --name shop_admin shop/admin:$image_version;
 # 查看日志
 docker logs shop_admin;
 #删除build过程中产生的镜像    #docker image prune -a -f
-docker rmi $(docker images -f "dangling=true" -q)
+docker rmi $(docker images -f "dangling=true" -q);
 # 对空间进行自动清理
-docker system prune -a -f
+docker system prune -a -f;
